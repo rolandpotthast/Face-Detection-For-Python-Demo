11582c71a00b7c9aea2bad5a934e6de94b64251f
Roland Potthast <roland.potthast@dwd.de>
2026-01-02 21:52:57 +0100

    Apply Black

--- face.py
+++ face.py
@@ -1,25 +1,28 @@
-
 from fdlite import FaceDetection, FaceDetectionModel
 from fdlite.render import Colors, detections_to_render_data, render_to_image
 import PIL
 
+
 def detect_faces(image: PIL.Image):
     detect_faces = FaceDetection(model_type=FaceDetectionModel.BACK_CAMERA)
-    faces=detect_faces(image)
+    faces = detect_faces(image)
     print(f"Found {len(faces)} faces")
     return faces
 
+
 def mark_faces(image_filename):
     """Mark all faces recognized in the image"""
-    image=PIL.Image.open(image_filename)
+    image = PIL.Image.open(image_filename)
 
-    faces=detect_faces(image)
+    faces = detect_faces(image)
 
     # Draw faces
-    render_data = detections_to_render_data(faces,bounds_color=Colors.GREEN,line_width=3)
-    render_to_image(     render_data, image)
+    render_data = detections_to_render_data(
+        faces, bounds_color=Colors.GREEN, line_width=3
+    )
+    render_to_image(render_data, image)
 
-    image.save(image_filename.with_suffix('.out.jpg'))
+    image.save(image_filename.with_suffix(".out.jpg"))
+
 
 mark_faces("Apollo_11_Crew.jpg")
-    
