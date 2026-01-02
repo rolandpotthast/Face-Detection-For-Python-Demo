86eec54200c469efec9316026602fa4839a40b02
Roland Potthast <43248098+rolandpotthast@users.noreply.github.com>
2026-01-02 22:28:52 +0100

    Refactor face detection functions and imports

--- face.py
+++ face.py
@@ -1,33 +1,33 @@
-#!/usr/bin/env python3
-from fdlite import FaceDetection, FaceDetectionModel
-from fdlite.render import Colors, detections_to_render_data, render_to_image
+from pathlib import Path
 
 import PIL
 import typer
-import pathlib
+from fdlite import FaceDetection, FaceDetectionModel
+from fdlite.render import Colors, detections_to_render_data, render_to_image
 
 
-def detect_faces(image: PIL.Image):
-    detect_faces = FaceDetection(model_type=FaceDetectionModel.BACK_CAMERA)
-    faces = detect_faces(image)
+def detect_faces(image: PIL.Image.Image):
+    detector = FaceDetection(model_type=FaceDetectionModel.BACK_CAMERA)
+    faces = detector(image)
     print(f"Found {len(faces)} faces")
     return faces
 
 
-def mark_faces(image_filename: pathlib.Path):
-    """Mark all faces recognized in the image"""
+def mark_faces(image_filename: Path):
+    """Mark all faces recognized in the image."""
     image = PIL.Image.open(image_filename)
 
     faces = detect_faces(image)
 
-    # Draw faces
     render_data = detections_to_render_data(
         faces, bounds_color=Colors.GREEN, line_width=3
     )
     render_to_image(render_data, image)
 
-    image.save(image_filename.with_suffix(".out.jpg"))
+    out_file = image_filename.with_name(image_filename.stem + "_out.jpg")
+    image.save(out_file)
+    print(f"Saved: {out_file}")
 
 
-if __name__ == '__main__': 
-    typer.run( mark_faces )
+if __name__ == "__main__":
+    typer.run(mark_faces)
